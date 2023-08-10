50b64d4122a63522f65d4fdc00d7a3b2f10b943d
Shivam Gupta <60308088+shivammgupta@users.noreply.github.com>
2023-08-10 20:51:30 +0530

    Update ForecastResponse.java

--- src/main/java/org/example/domain/ForecastResponse.java
+++ src/main/java/org/example/domain/ForecastResponse.java
@@ -2,6 +2,7 @@
 package org.example.domain;
 
 import java.util.ArrayList;
+import lombok.Data;
 
 public class ForecastResponse {
 
@@ -10,45 +11,5 @@ public class ForecastResponse {
     private City city;
     private Integer cnt;
     private java.util.List<Forecast> list = new ArrayList<>();
-
-    public String getCod() {
-        return cod;
-    }
-
-    public void setCod(String cod) {
-        this.cod = cod;
-    }
-
-    public Double getMessage() {
-        return message;
-    }
-
-    public void setMessage(Double message) {
-        this.message = message;
-    }
-
-    public City getCity() {
-        return city;
-    }
-
-    public void setCity(City city) {
-        this.city = city;
-    }
-
-    public Integer getCnt() {
-        return cnt;
-    }
-
-    public void setCnt(Integer cnt) {
-        this.cnt = cnt;
-    }
-
-    public java.util.List<Forecast> getList() {
-        return list;
-    }
-
-    public void setList(java.util.List<Forecast> list) {
-        this.list = list;
-    }
 
 }
